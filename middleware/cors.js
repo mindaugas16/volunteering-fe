73fb09700b4ed00dcba805fc4a61d435c9651333
mindaugas.morkunas <mindaugas.morkunas@mediapark.com>
2019-05-08 23:25:01 +0300

    FE and BE separated

--- middleware/cors.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS, PUT');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(200);
-    }
-    next();
-};
--- models/user-role.type.js
+++
@@ -1,9 +0,0 @@
-const UserRoles = {
-    GENERAL: 0,
-    VOLUNTEER: 1,
-    ORGANIZATION: 2,
-    SPONSOR: 3
-};
-module.exports = {
-    UserRoles
-};
