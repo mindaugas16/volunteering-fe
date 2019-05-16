44f1f17c16049b579433054b3eb6b240c3e68a5d
mindaugas.morkunas <mindaugas.morkunas@mediapark.com>
2019-05-16 15:47:50 +0300

    Server app listen fix

--- server.js
+++ server.js
@@ -13,6 +13,4 @@ app.get('/*', function(req,res) {
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Listening on http://localhost:` + port);
-});
+app.listen(process.env.PORT || 8080);
